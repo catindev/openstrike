7f278ba1e89f6e478ecc82487bc452a51753e167
Vladimir <catindev@gmail.com>
2026-06-14 13:17:45 +0200

    PR-01: add config loader skeleton

---
+++ src/core/config/config.gd
@@ -0,0 +1,7 @@
+extends Node
+
+class_name ConfigLoader
+
+static func load_config(path: String = "") -> Dictionary:
+    # Placeholder for loading configuration files. Not yet implemented.
+    return {}
